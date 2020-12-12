73736fb398f46b0877fb04a19f6745c11d0575a4
github username <github email address>
2020-12-11 17:37:12 -0800

    fixed comment validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -9,6 +9,4 @@ class Comment < ApplicationRecord
     validates :content, length: {maximum: 250} 
     validates :stars, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 6}
 
-
-    validates :content, uniqueness: { scope: :user, message: "has already been commented on"  }
 end
